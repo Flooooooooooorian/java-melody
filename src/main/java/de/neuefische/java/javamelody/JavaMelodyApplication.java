346d6dc2449d0d7f36efa0fa6829993ab369c910
Flooooooooooorian <florian.weber@neuefische.de>
2024-09-16 17:12:44 +0200

    Add melody

---
+++ src/main/java/de/neuefische/java/javamelody/JavaMelodyApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.java.javamelody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMelodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMelodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/java/javamelody/JavaMelodyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.java.javamelody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMelodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
